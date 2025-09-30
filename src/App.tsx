3deb8d88395ef724af6bb78e1d11b07f507aaf47
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2025-09-30 14:14:32 +0200

    Extracted EntryList to another file to make App.tsx clean and small before adding React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,11 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
+import EntryList from "./entries/EntryList.tsx"
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Nowy pomiar"),
-      isDone: true
-    });
-  }
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({id})
-  }
 
   return (
     <main>
       <h1>Duszności Wczesnej Dorosłości</h1>
-      <ul>
-        {todos.map(todo => <>
-              <li 
-                onClick={() => deleteTodo(todo.id)}
-                key={todo.id}>
-                {todo.content}
-              </li>
-              <input type="checkbox" id="vehicle1" name="vehicle1" value={todo.isDone}/>
-              <label htmlFor="vehicle1"> I have a bike</label>
-          </>
-          )}
-      </ul>
-      <button onClick={createTodo}>+</button>
+      <EntryList />
     </main>
   );
 }
---
+++ src/entries/EntryList.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+
+const client = generateClient<Schema>();
+
+function EntryList() {
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  useEffect(() => {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }, []);
+
+  function createTodo() {
+    client.models.Todo.create({
+      content: window.prompt("Nowy pomiar"),
+      isDone: true
+    });
+  }
+
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({id})
+  }
+
+  return (
+  	<>
+	  <ul>
+	    {todos.map(todo => <>
+	          <li 
+	            onClick={() => deleteTodo(todo.id)}
+	            key={todo.id}>
+	            {todo.content}
+	          </li>
+	          <input type="checkbox" id="vehicle1" name="vehicle1" value={todo.isDone}/>
+	          <label htmlFor="vehicle1"> I have a bike</label>
+	      </>
+	      )}
+	  </ul>
+	  <button onClick={createTodo}>+</button>
+    </>
+  );
+
+}
+
+export default EntryList;
