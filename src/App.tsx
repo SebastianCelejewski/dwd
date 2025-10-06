d719c50aae19714ba4b86d2363d2ffb41c3c6cf1
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2025-10-06 13:11:19 +0200

    Adding user nickname and application version below the application title

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,50 @@ import MeasurementList from "./measurements/measurementList.tsx"
 import MeasurementDetails from "./measurements/measurementDetails.tsx"
 import MeasurementAdd from "./measurements/measurementAdd.tsx"
 import { Authenticator } from '@aws-amplify/ui-react';
+import { fetchUserAttributes } from 'aws-amplify/auth';
+import { useState} from 'react'
 
 function App() {
+
+  const [userNickname, setUserNickname] = useState<string>([]);
+
+  function getUserNickname() {
+    fetchUserAttributes().then((attributes) => {
+      console.log(attributes.nickname)
+      if (attributes.nickname === undefined) {
+        setUserNickname("Anonim")
+      } else
+      {
+        setUserNickname(attributes.nickname)
+      }
+    })
+  }
+
+  getUserNickname()
 
   return (
     <Authenticator>
       {({ signOut, user }) => {
-        console.log(JSON.stringify(user))
         return ( 
-      <main>
-        <h1>Duszności Wczesnej Dorosłości</h1>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<MeasurementList />} />
-            <Route path="/measurements" element={<MeasurementList />} />
-            <Route path="/measurements/new" element={<MeasurementAdd />} />
-            <Route path="/measurements/:id" element={<MeasurementDetails />} />
-          </Routes>
-        </BrowserRouter>
-        <button onClick={signOut}>Wyloguj się</button>
-      </main>
-    )}}
+          <main>
+            <h1>Duszności Wczesnej Dorosłości</h1>
+            <div className="subheader">
+              <p className="userInfo">Witaj, {userNickname}</p>
+              <p className="versionInfo">Wersja 0.3</p>
+            </div>
+            <div style={{clear: 'both'}}/>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<MeasurementList />} />
+                <Route path="/measurements" element={<MeasurementList />} />
+                <Route path="/measurements/new" element={<MeasurementAdd />} />
+                <Route path="/measurements/:id" element={<MeasurementDetails />} />
+              </Routes>
+            </BrowserRouter>
+            <button onClick={signOut}>Wyloguj się</button>
+          </main>
+        )
+      }}
     </Authenticator>
   );
 }
